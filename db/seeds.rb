b0ac2cdcd064e3d8c2c099ec0275144ab8e1a86e
Josh Peterson <petersonjm1@gmail.com>
2021-02-07 10:05:29 -0500

    Add code to see the database with translations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require_relative '../external/BibleParser/lib/load.rb'
+
+translations = [
+    {:name => "Douay-Rheims", :file_name => "douay-rheims"},
+    {:name => "King James", :file_name => "king-james"},
+]
+
+translations.each do |translation|
+    puts "Loading translation #{translation[:name]}"
+    bible = load(translation[:file_name])
+    db_bible = Bible.create(name: translation[:name])
+    db_books = []
+    bible.books.each do |book|
+        puts "Processing book #{book.title} with #{book.chapters.length} chapters"
+        db_book = Book.create(title: book.title)
+        db_chapters = []
+        book.chapters.each do |chapter|
+            db_chapter = Chapter.create
+            db_verses = []
+            chapter.verses.each do |verse|
+                db_verse = Verse.create(text: verse.text)
+                db_verse.save
+                db_verses.append(db_verse)
+            end
+            db_chapter.verses = db_verses
+            db_chapter.save
+            db_chapters.append(db_chapter)
+        end
+        db_book.chapters = db_chapters
+        db_book.save
+        db_books.append(db_book)
+    end
+    puts "Finished processing books"
+    db_bible.books = db_books
+    db_bible.save
+    puts "Finished translation #{translation[:name]}"
+end
+puts "Done!"
---
+++ lib/tasks/db.rake
@@ -0,0 +1,7 @@
+# From : https://nithinbekal.com/posts/rake-db-reseed/
+namespace :db do
+  desc 'Drop, create, migrate then seed the development database'
+  task reseed: [ 'db:drop', 'db:create', 'db:migrate', 'db:seed' ] do
+    puts 'Reseeding completed.'
+  end
+end
